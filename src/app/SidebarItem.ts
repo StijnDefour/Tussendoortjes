a67ff83cc7fd2a3360c24adc7df662f99cb4c28b
StijnDefour <defourstijn10@gmail.com>
2018-12-04 21:31:31 +0100

    initial commit
    bevat al css voor navigatie

---
+++ src/app/SidebarItem.ts
@@ -0,0 +1,8 @@
+export class SidebarItem {
+  constructor(
+    public item_color: string,
+    public item_img: string,
+    public item_bg: string,
+    public item_beschrijving: string,
+    public item_themas: ThemaItem) { }
+}
---
+++ src/app/ThemaItem.ts
@@ -0,0 +1,6 @@
+export class ThemaItem {
+  constructor(
+    public item_titel: string,
+    public item_themas: Array<string>,
+    public item_urls: Array<string>) { }
+}
