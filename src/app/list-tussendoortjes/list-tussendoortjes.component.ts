e6a22c9350d294106ede38765813f9c99d13ec29
StijnDefour <defourstijn10@gmail.com>
2018-12-05 16:10:14 +0100

    Routing met params

--- src/app/list-tussendoortjes/list-tussendoortjes.component.ts
+++ src/app/list-tussendoortjes/list-tussendoortjes.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-list-tussendoortjes',
   templateUrl: './list-tussendoortjes.component.html',
-  styleUrls: ['./list-tussendoortjes.component.css']
+  styleUrls: ['./list-tussendoortjes.component.css'],
+
 })
 export class ListTussendoortjesComponent implements OnInit {
+  id: number;
+  private sub: any;
 
+  constructor(private route: ActivatedRoute) { }
 
-  constructor() { }
-
-  ngOnInit() {}
+  ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+  }
 
 }
